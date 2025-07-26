80354ecfa89eb421e1890a166645d4d62b3f8973
Anuj Dubey <dubey2175@gmail.com>
2025-07-27 00:01:58 +0530

    Bullet updated

--- gun.gd
+++ gun.gd
@@ -12,6 +12,8 @@ func shoot():
 	var BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
 	new_bullet.global_position = $Shootingpoint.global_position
+	new_bullet.global_rotation = $Shootingpoint.global_rotation
+	
 	$Shootingpoint.add_child(new_bullet)
 
 
